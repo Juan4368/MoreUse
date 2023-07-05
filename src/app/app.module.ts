36f2dd037c7831fc8ae806ddab81efa8855e9139
jveleva <jveleva@emtelco.com.co>
2023-07-04 19:05:06 -0500

    Primer carga del proyecto

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DefaultModule } from './UI/layouts/default/default.module';
+import { SharedModule } from './UI/shared/shared.module';
+import { FullscreenModule } from './UI/layouts/fullscreen/fullscreen.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    DefaultModule,
+    FullscreenModule,
+    SharedModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
