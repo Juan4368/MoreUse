1271c6a98e82b52afcc13c9dbb3f014a60e31a7a
jveleva <jveleva@emtelco.com.co>
2023-07-04 21:41:14 -0500

    Se agregan validaciones en formulario contactame

--- src/app/UI/components/contactus/contactus.component.ts
+++ src/app/UI/components/contactus/contactus.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contactus',
   templateUrl: './contactus.component.html',
   styleUrls: ['./contactus.component.css']
 })
-export class ContactusComponent {
+export class ContactusComponent implements OnInit { //inicializa el formulario cada vez que se ejecuta un cambio
+
+  contacusForm!: FormGroup;
+  constructor(private formBuilder: FormBuilder) { }
+  ngOnInit(): void {
+    this.contacusForm = this.formBuilder.group({
+      name: ['name', [
+        Validators.required
+      ]
+      ],
+      email: ['email', [
+        Validators.required,
+        Validators.email
+      ]
+      ],
+      message: ['message', [
+        Validators.required,
+        Validators.maxLength(260)
+      ]]
+    })
+  }
+
+  send() {
+    if (this.contacusForm.valid) {
+      alert('Mensaje enviado con exito')
+    }
+    else {
+      alert('valida por favor los campos');
+    }
+  }
 
 }
